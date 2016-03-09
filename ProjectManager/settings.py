5512ca6d3a3749abb168c25e18d985ef64fccdb9
Alexis Nowikowski <nowikowski22@hotmail.com>
2016-03-09 23:32:12 +0100

    Updated database connection settings

--- ProjectManager/settings.py
+++ ProjectManager/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ProjectManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project_manager',
+        'USER': 'postgres',
+        'PASSWORD': 'qweasd123!@#',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
