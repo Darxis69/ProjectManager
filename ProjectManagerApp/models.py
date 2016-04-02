60a72d58dce024bf1e7aeb17566f5c4b70a3aac3
pawello2222 <pawello2222@gmail.com>
2016-04-02 14:11:40 -0700

    Project template

--- ProjectManagerApp/models.py
+++ ProjectManagerApp/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Project(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-
-    #TODO powinno byc chyba tylko wolny/zajety
-    status = models.CharField(max_length=10)
-
-    #TODO
-    #team = models.OneToOneField(Team,unique=True)
-
 
 class UserBase(User):
     pass
@@ -28,13 +18,13 @@ class Team(models.Model):
     first_teammate = models.ForeignKey(Student, null=True, related_name="first_student", on_delete=models.SET_NULL)
     second_teammate = models.ForeignKey(Student, null=True, related_name="second_student", on_delete=models.SET_NULL)
 
+class Project(models.Model):
+    PROJECT_STATUS = (
+         ('O', 'Open'),
+         ('C', 'Closed')
+     )
 
-# class Project(models.Model):
-#     PROJECT_STATUS = (
-#         ('O', 'Open'),
-#         ('C', 'Closed')
-#     )
-#     project_name = models.CharField(max_length=30)
-#     project_description = models.CharField(max_length=200)
-#     status = models.CharField(max_length=1, choices=PROJECT_STATUS)
-#     assigned_team = models.OneToOneField(Team, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    status = models.CharField(max_length=1, choices=PROJECT_STATUS)
+    assigned_team = models.OneToOneField(Team, null=True, on_delete=models.SET_NULL)
