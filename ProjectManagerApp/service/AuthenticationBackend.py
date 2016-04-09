43178fe728c90a1a7fde16ee30787554453c88fd
Alexis Nowikowski <nowikowski22@hotmail.com>
2016-04-10 00:45:05 +0200

    Authentication - return derived class from UserBase

--- ProjectManagerApp/service/AuthenticationBackend.py
+++ ProjectManagerApp/service/AuthenticationBackend.py
@@ -1,20 +1,37 @@
-from ProjectManagerApp.models import UserBase
+from ProjectManagerApp.models import UserBase, Student, Teacher
 
 
 class AuthenticationBackend(object):
     def authenticate(self, username=None, password=None):
         try:
-            user = UserBase.objects.get(username=username)
+            user_base = UserBase.objects.get(username=username)
         except UserBase.DoesNotExist:
             return None
 
-        if user.check_password(password) is False:
+        if user_base.check_password(password) is False:
             return None
 
-        return user
+        return self.__map_base_user_to_derived_user(user_base)
 
     def get_user(self, user_id):
         try:
-            return UserBase.objects.get(pk=user_id)
+            user_base = UserBase.objects.get(pk=user_id)
         except UserBase.DoesNotExist:
             return None
+
+        return self.__map_base_user_to_derived_user(user_base)
+
+    def __map_base_user_to_derived_user(self, user_base):
+        try:
+            student = user_base.student
+            return student
+        except Student.DoesNotExist:
+            pass
+
+        try:
+            teacher = user_base.teacher
+            return teacher
+        except Teacher.DoesNotExist:
+            pass
+
+        raise NotImplementedError('Unknown user type')
