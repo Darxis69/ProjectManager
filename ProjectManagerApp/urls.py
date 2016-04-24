70d90091610db039df3f1dc0b2b762aa7a253ef0
Alexis Nowikowski <nowikowski22@hotmail.com>
2016-04-24 16:03:37 +0200

    Don't hardcode urls in templates

--- ProjectManagerApp/urls.py
+++ ProjectManagerApp/urls.py
@@ -5,16 +5,16 @@ from ProjectManagerApp.views import LoginFormView, AccountCreateFormView, Projec
 from . import views
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^index/$', IndexView.as_view(), name='index'),
-    url(r'^account/login/$', LoginFormView.as_view(), name='login'),
-    url(r'^account/logout/$', views.logout, name='logout'),
-    url(r'^account/create/$', AccountCreateFormView.as_view(), name="account_create"),
-    url(r'^projects/$', ProjectListView.as_view(), name="project_list"),
-    url(r'^projects/create/$', ProjectCreateFormView.as_view(), name="project_create"),
-    url(r'^projects/delete/$', views.project_delete, name="project_delete"),
-    url(r'^teams/$', TeamListView.as_view(), name="team_list"),
-    url(r'^teams/create/$', TeamCreateFormView.as_view(), name="team_create"),
-    url(r'^teams/join/$', views.team_join, name="team_join"),
-    url(r'^teams/leave/$', views.team_leave, name="team_leave")
+    url(r'^$', IndexView.as_view(), name='index_url'),
+    url(r'^index/$', IndexView.as_view(), name='index_url'),
+    url(r'^account/login/$', LoginFormView.as_view(), name='account_login_url'),
+    url(r'^account/logout/$', views.logout, name='account_logout_url'),
+    url(r'^account/create/$', AccountCreateFormView.as_view(), name="account_create_url"),
+    url(r'^projects/$', ProjectListView.as_view(), name="project_list_url"),
+    url(r'^projects/create/$', ProjectCreateFormView.as_view(), name="project_create_url"),
+    url(r'^projects/delete/$', views.project_delete, name="project_delete_url"),
+    url(r'^teams/$', TeamListView.as_view(), name="team_list_url"),
+    url(r'^teams/create/$', TeamCreateFormView.as_view(), name="team_create_url"),
+    url(r'^teams/join/$', views.team_join, name="team_join_url"),
+    url(r'^teams/leave/$', views.team_leave, name="team_leave_url")
 ]
