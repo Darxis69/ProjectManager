894e48d9f1622d1c9801ea6aed4a953941e73837
pawello2222 <pawello2222@gmail.com>
2016-05-04 20:51:20 +0200

    Updated models

--- ProjectManagerApp/models.py
+++ ProjectManagerApp/models.py
@@ -11,15 +11,25 @@ class Teacher(UserBase):
 
 
 class Student(UserBase):
+    STUDENT_STATUS_UNASSIGNED = 'U'
+    STUDENT_STATUS_UNASSIGNED_LABEL = 'Unassigned'
+    STUDENT_STATUS_ASSIGNED = 'A'
+    STUDENT_STATUS_ASSIGNED_LABEL = 'Assigned'
+    STUDENT_STATUS = (
+         (STUDENT_STATUS_UNASSIGNED, STUDENT_STATUS_UNASSIGNED_LABEL),
+         (STUDENT_STATUS_ASSIGNED, STUDENT_STATUS_ASSIGNED_LABEL)
+     )
+
     student_no = models.IntegerField(unique=True)
     team = models.ForeignKey('Team', null=True, related_name='+', on_delete=models.SET_NULL, unique=False)
-    # todo status
+    status = models.CharField(max_length=1, choices=STUDENT_STATUS)
     
 
 class Team(models.Model):
     name = models.CharField(max_length=50)
     first_teammate = models.OneToOneField(Student, null=True, related_name='+', on_delete=models.SET_NULL, unique=True)
     second_teammate = models.OneToOneField(Student, null=True, related_name='+', on_delete=models.SET_NULL, unique=True)
+    project = models.OneToOneField('Project', null=True, on_delete=models.SET_NULL)
 
 
 class Project(models.Model):
@@ -36,4 +46,5 @@ class Project(models.Model):
     description = models.CharField(max_length=200)
     status = models.CharField(max_length=1, choices=PROJECT_STATUS)
     assigned_team = models.OneToOneField(Team, null=True, on_delete=models.SET_NULL)
+    all_teams = models.ForeignKey(Team, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(Teacher, null=False, related_name="+", on_delete=models.CASCADE)
