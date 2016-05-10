0ba4b939913e3f4b0c84e2f61958217e1d56efc4
Alexis Nowikowski <nowikowski22@hotmail.com>
2016-05-10 17:12:53 +0200

    Added javascript code to make forms confirmable

---
+++ ProjectManagerApp/static/js/scripts.js
@@ -0,0 +1,11 @@
+function makeConfirmableForm(form) {
+    form.submit(function(e) {
+        var currentForm = this;
+        e.preventDefault();
+        bootbox.confirm("Are you sure?", function(result) {
+            if (result) {
+                currentForm.submit();
+            }
+        });
+    });
+}
