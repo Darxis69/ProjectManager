88dd8c685f570a46801d9bcd59c49df9b2823780
Alexis Nowikowski <nowikowski22@hotmail.com>
2016-05-11 10:24:14 +0200

    Added API for making confirmable links

--- ProjectManagerApp/static/js/scripts.js
+++ ProjectManagerApp/static/js/scripts.js
@@ -12,3 +12,18 @@ function makeConfirmableForms(forms, confirmationMessage) {
         });
     });
 }
+
+function makeConfirmableLinks(links) {
+    makeConfirmableLinksWithMessage(links, "Are you sure?");
+}
+function makeConfirmableLinksWithMessage(links, confirmationMessage) {
+    $(links).on("click", function(e) {
+        var link = $(this).attr("href"); // "get" the intended link in a var
+        e.preventDefault();
+        bootbox.confirm(confirmationMessage, function(result) {
+            if (result) {
+                document.location.href = link;  // if result, "set" the document location
+            }
+        });
+    });
+}
